66e007f4e749fa254e983a28a97770ef1f1d2d53
lookymlive <lookymlive@gmail.com>
2026-09-09 12:15:20 -0300

    refactor: improve secrets.h placeholder values with descriptive defaults

--- Code/secrets.h
+++ Code/secrets.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define WIFI_SSID       ""
-#define WIFI_PASSWORD   ""
-#define GROQ_API_KEY    "gsk_xxxxxxxxxxx"
+#define WIFI_SSID     "your_wifi_ssid"
+#define WIFI_PASSWORD "your_wifi_password"
+#define GROQ_API_KEY  "gsk_your_api_key_here"
